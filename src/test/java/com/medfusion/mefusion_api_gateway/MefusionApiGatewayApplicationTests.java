2144a1264bde4aee681c5fc8350a48273a8ecc22
adarshrishu <adarshrishu08@gmail.com>
2025-02-26 01:40:24 +0530

    First Commit.

---
+++ src/test/java/com/medfusion/mefusion_api_gateway/MefusionApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medfusion.mefusion_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class MefusionApiGatewayApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
